4b086579d748339245fe376884df8a024e91d66b
Ayrton Teshima <ayrtonteshima@gmail.com>
2020-08-15 09:20:36 -0300

    arquivos das últimas aulas

--- tools/lib.js
+++ tools/lib.js
@@ -25,3 +25,127 @@ export function partial(fn, ...args) {
     return fn(...args, ...newArgs)
   }
 }
+
+export function map(fn) {
+  return function(arr) {
+    return arr.map(fn);
+  }
+}
+
+export function prop(key) {
+  return function(obj) {
+    return obj[key];
+  }
+}
+
+export function multiply(multi) {
+  return function(numero) {
+    return numero * multi
+  };
+}
+
+export function sum (arr) {
+  return arr.reduce(function(acc, valorCorrente) {
+    return acc + valorCorrente;
+  }, 0);
+}
+
+export function curry(fn,arity = fn.length) {
+  return (function nextCurried(prevArgs){
+      return function curried(nextArg){
+          var args = [ ...prevArgs, nextArg ];
+
+          if (args.length >= arity) {
+              return fn( ...args );
+          }
+          else {
+              return nextCurried( args );
+          }
+      };
+  })( [] );
+}
+
+
+export function add (a) {
+  return function(b) {
+    return a + b;
+  }
+}
+
+export function filter(fn) {
+  return function (arr) {
+    return arr.filter(fn);
+  }
+}
+
+export function isOdd(n) {
+  return n % 2 !== 0;
+}
+
+export function isEven(n) {
+  return n % 2 === 0;
+}
+
+export function gt(n1) {
+  return function (n2) {
+    return n2 > n1;
+  }
+}
+
+export function gte(n1) {
+  return function (n2) {
+    return n2 >= n1;
+  }
+}
+
+export function lt(n1) {
+  return function (n2) {
+    return n2 < n1;
+  }
+}
+
+export function lte(n1) {
+  return function (n2) {
+    return n2 <= n1;
+  }
+}
+
+export function trim(text) {
+  return text.trim();
+}
+
+export function toUpper(text) {
+  return text.toUpperCase();
+}
+
+export function compose(...fns) {
+  return function (x) {
+    return fns.reduceRight(function(y, f) {
+      return f(y)
+    }, x)
+  }
+}
+
+export function composeP(...fns) {
+  return function (x) {
+    return fns.reduceRight(function(y, f) {
+      return Promise.resolve(y).then(f)
+    }, x)
+  }
+}
+
+export function pipe(...fns) {
+  return function (x) {
+    return fns.reduce(function(y, f) {
+      return f(y)
+    }, x)
+  }
+}
+
+export function pipeP(...fns) {
+  return function (x) {
+    return fns.reduce(function(y, f) {
+      return Promise.resolve(y).then(f)
+    }, x)
+  }
+}
